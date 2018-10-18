5da715fa6968a3c3192991f54e804996605be570
release <release@apptio.com>
2018-10-18 08:35:46 -0700

    Brew formula update for kr8 version v0.0.1

--- kr8.rb
+++ kr8.rb
@@ -3,7 +3,7 @@ class Kr8 < Formula
   homepage "https://github.com/apptio/kr8"
   url "https://github.com/apptio/kr8/releases/download/v0.0.1/kr8_0.0.1_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "a66f37cf39e5141a21dca8a153dcb41b52d85367aeb526ea82d7a0653f86d0c5"
+  sha256 "30b10494205bf7ca27444750b50a327b4a604e96d0c4bb4db7e6ab563e7a607f"
 
   def install
     bin.install "kr8"
